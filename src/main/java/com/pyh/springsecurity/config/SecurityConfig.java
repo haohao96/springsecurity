42b4addad82a2c881fc77ad8f1f97881485037c2
心若止水 <18437927639@163.com>
2021-08-03 00:33:33 +0800

    config

--- src/main/java/com/pyh/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/pyh/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pyh.springsecurity.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,8 +15,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/boss").permitAll() // "/"路径允许任何人访问
                 .antMatchers("/content").hasRole("boss"); //
 //        开启没有权限时自动跳转的登录页面
-        http.formLogin()
-                .usernameParameter("springboot")
-                .passwordParameter("springsecurity");
+        http.formLogin();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("springboot")
+                .password("springsecurity").roles("boss");
+
     }
 }
